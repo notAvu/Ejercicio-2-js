4bbf7e327d9ba1bea56287207c1c11bb6e8f4a71
Álvaro Fernández <avunotfound@gmail.com>
2022-01-13 10:51:30 +0100

    Funsiona

--- ScriptLista.js
+++ ScriptLista.js
@@ -7,21 +7,39 @@ function inicializaEventos()
     // foreach(Element rb in RadioButton){
     //     rb.Attributes.Add("onclick", "alert('hello');");
     // }
-        document.getElementById("butno").addEventListener("click", getCharacterList, false);
+        document.getElementById("guilty").addEventListener("change", getGgCharacterList, false);
+        document.getElementById("blaz").addEventListener("change", getBbCharacterList, false);
         
 }
-function getCharacterList()
+function getGgCharacterList()
 {
-    if(document.click('input[name="blaz"]:checked').value)
+    if(document.getElementById("guilty").checked)
     {
+        document.getElementById("blaz").checked=false;
+        document.getElementById("char1").innerHTML=ggCharacters[0];
+        document.getElementById("char2").innerHTML=ggCharacters[1];
+        document.getElementById("char3").innerHTML=ggCharacters[2];
+    }else
+    {
+        empty();
+    }
+}
+function getBbCharacterList()
+{
+    if(document.getElementById("blaz").checked)
+    {
+        document.getElementById("guilty").checked=false;
         document.getElementById("char1").innerHTML=bbCharacters[0];
         document.getElementById("char2").innerHTML=bbCharacters[1];
         document.getElementById("char3").innerHTML=bbCharacters[2];
     }else
     {
-        document.getElementById("char1").innerHTML=ggCharacters[0];
-        document.getElementById("char2").innerHTML=ggCharacters[1];
-        document.getElementById("char3").innerHTML=ggCharacters[2];
+        empty();
     }
-    
+}
+function empty()
+{
+    document.getElementById("char1").innerHTML=""
+    document.getElementById("char2").innerHTML=""
+    document.getElementById("char3").innerHTML=""
 }
